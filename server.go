b0bf05c24fdc054f0235fd4d1f8b860d39843039
aeramu <sulam3010@gmail.com>
2021-01-27 11:46:32 +0700

    fix empty variables

--- server.go
+++ server.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+
 	"github.com/aeramu/graphrpc/proto"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-type Server struct{
+type Server struct {
 	Schema *graphql.Schema
 }
 
@@ -18,18 +21,22 @@ func NewGraphRPCServer(schema *graphql.Schema) *grpc.Server {
 	baseServer := grpc.NewServer()
 	server := &Server{Schema: schema}
 	proto.RegisterGraphRPCServer(baseServer, server)
+	reflection.Register(baseServer)
 	return baseServer
 }
 
 func (h *Server) Exec(ctx context.Context, request *proto.ExecRequest) (*proto.ExecResponse, error) {
 	variables := map[string]interface{}{}
-	if err := json.Unmarshal(request.GetVariables(), &variables); err != nil {
-		return nil, err
+	if request.GetVariables() != nil {
+		if err := json.Unmarshal(request.GetVariables(), &variables); err != nil {
+			log.Println("Failed unmarshal JSON variables:", err)
+			return nil, err
+		}
 	}
 
 	res := h.Schema.Exec(ctx, request.GetQuery(), request.GetOperationName(), variables)
 	return &proto.ExecResponse{
-		Data:  res.Data,
+		Data:   res.Data,
 		Errors: parseErrors(res.Errors),
 	}, nil
 }
@@ -37,9 +44,9 @@ func (h *Server) Exec(ctx context.Context, request *proto.ExecRequest) (*proto.E
 func parseErrors(errs []*errors.QueryError) (protoErrors []*proto.Error) {
 	for _, err := range errs {
 		protoErrors = append(protoErrors, &proto.Error{
-			Message: err.Message,
+			Message:   err.Message,
 			Locations: parseLocations(err.Locations),
-			Path: parsePath(err.Path),
+			Path:      parsePath(err.Path),
 		})
 	}
 	return
